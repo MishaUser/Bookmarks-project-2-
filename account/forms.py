c51dd9518c3a58cca194b2eeb707d45ad6e4e34c
misha <u-mi21@yandex.ru>
2023-11-23 13:43:55 +0300

    Разработка конкретно-прикладного бэкенда аутентификации

--- account/forms.py
+++ account/forms.py
@@ -23,12 +23,30 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
     
+    # Не позволяет регистрироваться с уже существующим 
+    # адресом эл.почты.
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+    
 # Позволяет редактировать свое имя, 
-# фамилию и адрес электронной почты.
+# фамилию и адрес эл.почты.
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    # Не позволяет изменять свой бывший адрес эл.почты
+    # на существующий адрес эл.почты.
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id)\
+                         .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError(' Email alredy in use.')
+        return data
 
 # Пользователи смогут редактировать дату своего рождения 
 # и закачивать изоражение на сайт в качестве фотоснимка профиля. 
