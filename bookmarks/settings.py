0ee10885a51e23c8e10d28521850409d57f2783c
misha <u-mi21@yandex.ru>
2023-11-25 14:45:37 +0300

    Добавление социальной аутентификации на сайт

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=!^9&e!i2iy7^8yz83r=z)mh+fvw9oacpm7@qzh1u*bpc^v^r1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
