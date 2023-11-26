0b406d949ff031288d4098fc0e99390bb5b62360
misha <u-mi21@yandex.ru>
2023-11-26 15:18:08 +0300

    Создание представления детальной инормации об изображениях

--- images/models.py
+++ images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 # Модель для хранения изображений на платформе.
 class Image(models.Model):
@@ -31,5 +33,9 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id,
+                                              self.slug])
         
 
